a9897e524e01dce48c0337a448f97eddc4de6bf9
Nick Drishinski <ndrishinski@yahoo.com>
2019-09-14 13:23:59 -0700

    adding to tail

--- public/js/Snake.js
+++ public/js/Snake.js
@@ -51,12 +51,20 @@ export default class Snake extends Phaser.Scene {
 
     move() {
 
+        let x = this.body[0].x + this.direction.x * this.tileSize
+        let y = this.body[0].y + this.direction.y * this.tileSize
+
+        if (this.apple.x == x && this.apple.y == y) {
+            this.body.push(this.scene.add.rectangle(0,0,this.tileSize, this.tileSize, 0xffffff).setOrigin(0))
+            this.positionApple();
+        }
+
         for (let i = this.body.length -1; i > 0; i--) {
             console.log(i)
             this.body[i].x = this.body[i -1].x;
             this.body[i].y = this.body[i -1].y;
         }
-        this.body[0].x += this.direction.x * 16;
-        this.body[0].y += this.direction.y * 16;
+        this.body[0].x = x;
+        this.body[0].y = y;
     }
 }
