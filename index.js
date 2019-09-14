ec4a6614578ac1d83e6f6545402f8705783864ce
Nick Drishinski <ndrishinski@yahoo.com>
2019-09-13 17:52:31 -0700

    added express and routes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('hello world')
+    res.sendFile(__dirname + '/public/index.html');
 })
 
 app.listen(port, () => console.log(`running on port: ${port}`))
